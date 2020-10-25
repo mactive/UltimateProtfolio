2c681da2112810025a68a8a15dc016f062e96f69
mactive <mactive@gmail.com>
2020-10-26 07:38:07 +0800

    Initial Commit

---
+++ UltimateProtfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UltimateProtfolio
+//
+//  Created by Qian Meng on 2020/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UltimateProtfolio/UltimateProtfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateProtfolioApp.swift
+//  UltimateProtfolio
+//
+//  Created by Qian Meng on 2020/10/26.
+//
+
+import SwiftUI
+
+@main
+struct UltimateProtfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
